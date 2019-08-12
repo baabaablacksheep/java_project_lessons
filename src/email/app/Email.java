a82ce1a6e4d45d6bddea6bb7493dd64e81d472d5
sahan.d <sahan.d@creativesoftware.com>
2019-08-12 18:36:58 +0530

    Completed Generating Email and Random Password

--- src/email/app/Email.java
+++ src/email/app/Email.java
@@ -1,10 +1,85 @@
 package email.app;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
 class Email {
-    Email(){
-        System.out.println("Email Object Created!");
+
+    // Basic Info of the User
+    private String firstName;
+    private String lastName;
+    private String company;
+    private String department;
+
+    // Account Attributes
+    private String emailAddress;
+    private String password;
+
+    private int mailboxCapacity = 100;
+    private String alternateEmail;
+
+    // Creating A Random Object To Generate Random Objects
+    private Random rand = new Random();
+
+    // Multiple Constructors to Handle Different Object Initializations
+    Email(@NotNull String firstName, @NotNull String lastName, @NotNull String company){
+        this(firstName,lastName,company,"");
     }
-    void printSomething(){
-        System.out.println("This is The Email Class...");
+    Email(@NotNull String firstName, @NotNull String lastName, @NotNull String company, String department){
+        this.firstName = firstName.toLowerCase();
+        this.lastName = lastName.toLowerCase();
+        this.company = company.toLowerCase();
+        this.department = department.toLowerCase();
+
+        this.emailAddress = generateEmailAddress();
+        this.password = generatePassword();
+    }
+
+    // Setters
+    public void setPassword(String oldPassword, String Newpassword) throws Exception{
+        if(this.password.equals(oldPassword)){
+            this.password = Newpassword;
+        } else {
+            throw new IllegalArgumentException("The Provided Password Doesn't Match With The Existing Password");
+        }
+    }
+    public void setMailboxCapacity(int mailboxCapacity) {
+        this.mailboxCapacity = mailboxCapacity;
+    }
+
+    public void setAlternateEmail(String alternateEmail) {
+        this.alternateEmail = alternateEmail;
+    }
+
+    // Getters
+    String getEmailAddress() {
+        return emailAddress;
+    }
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    // Methods Implementing Business Logic
+    private String generateEmailAddress(){
+        return this.firstName+"."+this.lastName+"@"+(!this.department.equals("") ? this.department +"." : "")+this.company+".com";
+    }
+    private String generatePassword(){
+
+        // Password Related
+        int passwordLength = 10;
+        int pwStartChar = 48;
+        int pwEndChar = 127;
+
+        StringBuilder strBuilder = new StringBuilder();
+        for(int i=0; i<passwordLength; i++){
+            int charNum = rand.nextInt(pwEndChar - pwStartChar) - pwStartChar;
+            strBuilder.append((char)charNum);
+        }
+        return strBuilder.toString();
     }
 }
--- src/email/app/EmailApp.java
+++ src/email/app/EmailApp.java
@@ -3,7 +3,11 @@ package email.app;
 public class EmailApp {
 
     public static void main(String[] args) {
-        Email email = new Email();
-        email.printSomething();
+        Email email_1 = new Email("Kamal","Ranasinghe","AxelLanka", "Sales");
+        Email email_2 = new Email("Amal","Gunasekara","ABCompany");
+
+        System.out.println("Email 1 : "+email_1.getEmailAddress());
+        System.out.println("Email 2 : "+email_2.getEmailAddress());
+
     }
 }
