283147003ea13dc3e1f09f5cd11a96983527a704
sahan.d <sahan.d@creativesoftware.com>
2019-08-12 17:47:00 +0530

    First Commit On The email-app branch

---
+++ src/email/app/Email.java
@@ -0,0 +1,10 @@
+package email.app;
+
+class Email {
+    Email(){
+        System.out.println("Email Object Created!");
+    }
+    void printSomething(){
+        System.out.println("This is The Email Class...");
+    }
+}
