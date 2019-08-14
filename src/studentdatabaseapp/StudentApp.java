756fd14498f5ff2c478fbb5ea129483b4d392f96
baabaablacksheep <sahanpoornaka1@gmail.com>
2019-08-14 09:53:11 +0530

    Working Program With All The Features Implemented

--- src/studentdatabaseapp/StudentApp.java
+++ src/studentdatabaseapp/StudentApp.java
@@ -1,14 +1,25 @@
 package studentdatabaseapp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentApp {
 
     public static void main(String[] args) {
 
-        // TODO Ask User How Many Students are Going to be Inserted
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number of Students To Be Registered: ");
+        int numStudents = sc.nextInt();
 
-        // TODO Create and Student Object
+        for (int i = 0; i < numStudents; i++) {
+            Student stud = new Student();
+            students.add(stud);
+        }
 
-        // TODO Keep The Created Student Object in an Array
+        for (Student student : students) {
+            int newBalance = student.payTutionFees(200);
+            System.out.println("New Balance: "+ newBalance);
+        }
     }
-
 }
